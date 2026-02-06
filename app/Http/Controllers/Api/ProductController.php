6cc893088747fccd84b99ef8b221f44444bd866c
Akash987099 <akashkumarsci233@gmail.com>
2026-02-07 01:08:32 +0530

    url fix

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -12,6 +12,7 @@ use App\Models\Product;
 use Milon\Barcode\DNS1D;
 use App\Models\Gallery;
 use App\Models\Summer;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -19,28 +20,35 @@ class ProductController extends Controller
     {
         try {
             $data = Summer::whereNotNull('position')
-            ->whereHas('products')->with(['products' => function ($query) {
-                $query
-                    ->leftJoin('discounts', 'discounts.id', '=', 'products.discount')
-                    ->leftJoin('brands', 'brands.id', '=', 'products.brands')
-                    ->select(
-                        'products.id as url',
-                        'products.name',
-                        'products.image',
-                        'products.price',
-                        'products.ac_price',
-                        'products.stock',
-                        'products.in_stock',
-                        'products.summer_id',
-                        'products.slug',
-                        'discounts.name as discount',
-                        'brands.name as brand'
-                    );
-            }])
+                ->whereHas('products')->with(['products' => function ($query) {
+                    $query
+                        ->leftJoin('discounts', 'discounts.id', '=', 'products.discount')
+                        ->leftJoin('brands', 'brands.id', '=', 'products.brands')
+                        ->select(
+                            'products.id as url',
+                            'products.name',
+                            'products.image',
+                            'products.price',
+                            'products.ac_price',
+                            'products.stock',
+                            'products.in_stock',
+                            'products.summer_id',
+                            'products.slug',
+                            'discounts.name as discount',
+                            'brands.name as brand'
+                        );
+                }])
                 ->select('id', 'name', 'position', 'image as banner')
                 ->whereNotNull('position')
                 ->orderBy('position', 'ASC')
                 ->get();
+
+            $data->each(function ($summer) {
+                $summer->products->each(function ($product) {
+                    $product->url = Str::slug($product->name) . '-' . $product->url;
+                    unset($product->id);
+                });
+            });
 
             return response()->json([
                 'status' => true,
@@ -74,6 +82,13 @@ class ProductController extends Controller
                     'brands.name as brand'
                 )
                 ->get();
+
+            $products->each(function ($summer) {
+                $summer->products->each(function ($product) {
+                    $product->url = Str::slug($product->name) . '-' . $product->url;
+                    unset($product->id);
+                });
+            });
 
             if ($products->isEmpty()) {
                 return response()->json([
