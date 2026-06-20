e9e14165e8b1df2594f297a6faba24d31af4d45a
Akash987099 <akashkumarsci233@gmail.com>
2026-06-20 22:48:26 +0530

    add recently product api

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -158,9 +158,9 @@
   <!--   Core JS Files   -->
   <script src="{{asset('assets/admin/js/core/popper.min.js')}}"></script>
   <script src="{{asset('assets/admin/js/core/bootstrap.min.js')}}"></script>
-  <script src="{{asset('assets/admin/js/plugins/perfect-scrollbar.min.js')}}"></script>
+  {{-- <script src="{{asset('assets/admin/js/plugins/perfect-scrollbar.min.js')}}"></script>
   <script src="{{asset('assets/admin/js/plugins/smooth-scrollbar.min.js')}}"></script>
-  <script src="{{asset('assets/admin/js/plugins/chartjs.min.js')}}"></script>
+  <script src="{{asset('assets/admin/js/plugins/chartjs.min.js')}}"></script> --}}
   
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
